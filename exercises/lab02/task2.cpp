0e9969d44493758e18e6a8e6243f3a95267314c5
Alexandar Galabov <alexandar_1997@abv.bg>
2020-03-05 23:04:19 +0200

    Add more examples for template function usage

--- exercises/lab02/task2.cpp
+++ exercises/lab02/task2.cpp
@@ -1,21 +1,43 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
+#include "helperFunctions.h"
 
-template<typename T>
-void replace(T* array,size_t sz, T toReplace, T replaceWith){
-  for(size_t i = 0; i < sz; ++i){
-    if(array[i] == toReplace){
+template <typename T>
+void replace(T *array, size_t sz, T toReplace, T replaceWith)
+{
+  for (size_t i = 0; i < sz; ++i)
+  {
+    if (array[i] == toReplace)
+    {
       array[i] = replaceWith;
     }
   }
 }
 
-int main(){
+int main()
+{
+
+  bool testBoolArray[] = {true, false, true};
+  printArray(testBoolArray);
+
+  replace(testBoolArray, 3, false, true);
+  printArray(testBoolArray);
+
+  printSeparationLine();
+
   char testString[] = "Replacing a with b";
   std::cout << testString << std::endl;
 
   replace(testString, strlen(testString), 'a', 'b');
   std::cout << testString << std::endl;
+
+  printSeparationLine();
+
+  int testIntArray[] = {1, 2, 3, 2, 5};
+  printArray(testIntArray);
+
+  replace(testIntArray, 5, 2, 6);
+  printArray(testIntArray);
 
   return 0;
 }

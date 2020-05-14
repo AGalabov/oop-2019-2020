8997e5c35d2fa4f8beed465d59217664567aa22a
AGalabov <alexandar_1997@abv.bg>
2020-05-14 21:06:54 +0300

    Format lab06 solutions

--- exercises/lab06/solutions.cpp
+++ exercises/lab06/solutions.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <functional>
- 
+
 // A function that accepts an integer and return an integer
 using intToInt = std::function<int(int)>;
 
@@ -8,35 +8,40 @@ using intToInt = std::function<int(int)>;
 using intToFunctionOfAnotherInt = std::function<intToInt(int)>;
 
 // Task 1:
-intToInt multiplyBy(int x) {
-    return [x] (int a) -> int{return x * a;};
+intToInt multiplyBy(int x)
+{
+    return [x](int a) -> int { return x * a; };
 }
- 
+
 // Task 2;
-intToFunctionOfAnotherInt multiplyByAgain(int y) {
-    return [y] (int x) {
-        return [x, y] (int a) { return x*y*a; };
+intToFunctionOfAnotherInt multiplyByAgain(int y)
+{
+    return [y](int x) {
+        return [x, y](int a) { return x * y * a; };
     };
 }
 
 // Task 3
 // A function that accepts an integer by reference and doesn not return anything (void)
-using intRefToVoid = std::function<void(int&)>;
+using intRefToVoid = std::function<void(int &)>;
 
-intRefToVoid refMultiplyBy(int x) {
-    return [x] (int& a) { a *= x;};
+intRefToVoid refMultiplyBy(int x)
+{
+    return [x](int &a) { a *= x; };
 }
 
-void forEach(int* arr, size_t size, intRefToVoid func)
+void forEach(int *arr, size_t size, intRefToVoid func)
 {
-    for(size_t i=0; i<size; ++i)
+    for (size_t i = 0; i < size; ++i)
     {
         func(arr[i]);
     }
 }
- 
-int main() {
-    std::cout << "=========================TASK1======================" << std::endl << std::endl;
+
+int main()
+{
+    std::cout << "=========================TASK1======================" << std::endl
+              << std::endl;
     int a = 3;
 
     // Creates callable objects (functions) of type intToInt
@@ -45,7 +50,8 @@ int main() {
 
     std::cout << multBy3(a) << " " << multBy5(a) << std::endl;
 
-    std::cout << "=========================TASK2======================" << std::endl << std::endl;
+    std::cout << "=========================TASK2======================" << std::endl
+              << std::endl;
 
     // Creates a callable object (a function) of type intToFunctionOfAnotherInt
     intToFunctionOfAnotherInt multBy2 = multiplyByAgain(2);
@@ -55,23 +61,23 @@ int main() {
 
     std::cout << multBy2And4(a) << " " << multBy2And7(a) << std::endl;
 
-
-    std::cout << "=========================TASK3======================" << std::endl << std::endl;
+    std::cout << "=========================TASK3======================" << std::endl
+              << std::endl;
 
     size_t size;
-    
+
     std::cout << "Enter size: ";
     std::cin >> size;
 
-    int* arr = new int[size];
+    int *arr = new int[size];
 
     // Reads all elements of the array.
     // Note the reference passing.
-    forEach(arr, size, [] (int& a) { std::cin >> a;});
+    forEach(arr, size, [](int &a) { std::cin >> a; });
 
     // Prints all elemens of the array with spaces
     std::cout << "Start array: ";
-    forEach(arr, size, [] (int& a) { std::cout << a << " ";});
+    forEach(arr, size, [](int &a) { std::cout << a << " "; });
     std::cout << std::endl;
 
     // Use a function created in the previous tasks
@@ -80,7 +86,7 @@ int main() {
 
     // Prints all elemens of the array with commas (very simple to change the way we print stuff)
     std::cout << "No change in the array: ";
-    forEach(arr, size, [] (int& a) { std::cout << a << ",";});
+    forEach(arr, size, [](int &a) { std::cout << a << ","; });
     std::cout << std::endl;
 
     intRefToVoid refMultBy3 = refMultiplyBy(3);
@@ -89,10 +95,10 @@ int main() {
 
     // Prints all elemens of the array
     std::cout << "Changed array: ";
-    forEach(arr, size, [] (int& a) { std::cout << a << " ";});
+    forEach(arr, size, [](int &a) { std::cout << a << " "; });
     std::cout << std::endl;
 
-    delete [] arr;
+    delete[] arr;
 
     return 0;
- }
+}
--- exercises/lab06/students.cpp
+++ exercises/lab06/students.cpp
@@ -2,21 +2,22 @@
 #include <fstream>
 #include <functional>
 
-struct Student{
+struct Student
+{
     double mark;
     long fn;
     bool failed;
 };
- 
-using comparatorFunction = std::function<bool(Student,Student)>;
- 
-void sort(Student* arr, size_t size, comparatorFunction func)
+
+using comparatorFunction = std::function<bool(Student, Student)>;
+
+void sort(Student *arr, size_t size, comparatorFunction func)
 {
-    for(size_t i=0; i < size - 1; ++i)
+    for (size_t i = 0; i < size - 1; ++i)
     {
-        for(size_t j = i + 1; j<size; ++j)
+        for (size_t j = i + 1; j < size; ++j)
         {
-            if(func(arr[i], arr[j]))
+            if (func(arr[i], arr[j]))
             {
                 std::swap(arr[i], arr[j]);
             }
@@ -24,15 +25,16 @@ void sort(Student* arr, size_t size, comparatorFunction func)
     }
 }
 
-void forEach(Student* arr, size_t size, std::function<void(Student&)> func)
+void forEach(Student *arr, size_t size, std::function<void(Student &)> func)
 {
-    for(size_t i=0; i<size; ++i)
+    for (size_t i = 0; i < size; ++i)
     {
         func(arr[i]);
     }
 }
 
-int main() {
+int main()
+{
 
     // I am using a .txt file so that you can easily read the conent of the uploaded file
     // Try to do it with a binary file
@@ -40,10 +42,10 @@ int main() {
 
     size_t size;
     in >> size;
- 
-    Student* arr = new Student[size];
- 
-    for(size_t i=0; i<size; ++i)
+
+    Student *arr = new Student[size];
+
+    for (size_t i = 0; i < size; ++i)
     {
         in >> arr[i].fn;
         in >> arr[i].mark;
@@ -51,34 +53,34 @@ int main() {
     }
 
     in.close();
-    
+
     // Sets a flag for std::cout to print bool values as true/false and not 1/0
     std::cout << std::boolalpha;
 
     std::cout << "\n==========Initial array=============\n";
-    forEach(arr, size,  [] (Student a) { 
+    forEach(arr, size, [](Student a) {
         std::cout << a.fn << " " << a.mark << " " << a.failed << std::endl;
-     });
- 
+    });
+
     std::cout << "\n==========Sorted in ascending order by fn=============\n";
-    sort(arr, size, [] (Student a,Student b) { return a.fn > b.fn; });
-    forEach(arr, size,  [] (Student a) { 
+    sort(arr, size, [](Student a, Student b) { return a.fn > b.fn; });
+    forEach(arr, size, [](Student a) {
         std::cout << a.fn << " " << a.mark << " " << a.failed << std::endl;
-     });
+    });
 
     std::cout << "\n==========Sorted in descending order by mark=============\n";
-    sort(arr, size, [] (Student a,Student b) { return a.mark < b.mark; });
-    forEach(arr, size,  [] (Student a) { 
+    sort(arr, size, [](Student a, Student b) { return a.mark < b.mark; });
+    forEach(arr, size, [](Student a) {
         std::cout << a.fn << " " << a.mark << " " << a.failed << std::endl;
-     });
+    });
 
     std::cout << "\n==========Sorted - passed first=============\n";
-    sort(arr, size, [] (Student a,Student b) { return a.failed < b.failed; });
-    forEach(arr, size,  [] (Student a) { 
+    sort(arr, size, [](Student a, Student b) { return a.failed < b.failed; });
+    forEach(arr, size, [](Student a) {
         std::cout << a.fn << " " << a.mark << " " << a.failed << std::endl;
-     });
- 
-    delete [] arr;
+    });
+
+    delete[] arr;
 
     return 0;
 }
